5a7d97e72e922a0e5f001d04e8c3a3be77f28239
nandan340 <deval340@gmail.com>
2018-07-24 01:49:12 -0400

    Add files via upload

--- doctortable.php
+++ doctortable.php
@@ -44,8 +44,9 @@ while (($row = mysql_fetch_array($result, MYSQL_ASSOC)) !== false){
     </form>
 
 </div>
-
+<?php if(!empty($data)){?>
 <div id="db_table">
+
 <table class="table table-bordered" width="100%">
     <thead>
 
@@ -204,7 +205,10 @@ while (($row = mysql_fetch_array($result, MYSQL_ASSOC)) !== false){
 </table>
 </div>
 
+<?php }else{?>
+<h5>No record Found !!</h5>
 
+<?php }?>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
